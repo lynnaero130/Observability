ee0b0a8896e958e06bdc206cc78bc2fb53486dca
Lynn <lynnaero@sjtu.edu.cn>
2021-04-13 15:54:02 +0800

    revised plot method

---
+++ experiment/LSR.m
@@ -0,0 +1,14 @@
+% This script is used to calculate the offset of ground truth
+x0 = [0;0;0];
+options = optimoptions('fmincon','Algorithm','sqp','MaxFunctionEvaluations',200000);
+X = fmincon(@(x)obj (x,y,gtd),x0,[],[],[],[],[],[],[],options);
+
+function f = obj(x,uwb,gtd)
+a = x(1);
+b = x(2);
+c = x(3);
+temp = (uwb - sqrt((gtd(1,:)-a).^2 + (gtd(2,:)-b).^2 + (gtd(3,:)-c).^2)).^2;
+f = sum(temp,2);
+end
+
+
