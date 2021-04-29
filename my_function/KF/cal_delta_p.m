3af9e3a6f0678636a7884e12d68d418fe83b06bb
lynnaero130 <lynnaero@sjtu.edu.cn>
2021-04-29 16:26:23 +0800

    KF with moving horizon

---
+++ my_function/KF/cal_delta_p.m
@@ -0,0 +1,16 @@
+function delta_p = cal_delta_p(imu,dt)
+% this function is used to calculate delta_p
+        K = size(imu,2);
+        delta_v(:,1) = imu(:,1)*dt;
+        for i = 2:K
+            delta_v(:,i) = delta_v(:,i-1) + imu(:,i)*dt;
+        end
+
+         delta_p(:,1) = 0.5*dt^2*imu(:,i); % the position variance rised from acc.
+        for i = 2:K
+            delta_p(:,i) = delta_p(:,i-1) + delta_v(:,i-1)*dt + 0.5*dt^2*imu(:,i);
+        end
+
+
+end
+
--- simulation/circular.m
+++ simulation/circular.m
@@ -2,32 +2,32 @@
 %% 1. Initialize
 clc;close all
 clearvars -except imu_noise uwb_noise K dt sigma_omega sigma_v gain
-x0 = [51;1;4;0;0;0]; % use prediction as initial guess
+x0 = [51;1;0;0;0]; % use prediction as initial guess
 xg = [51;1;-300;0;0;0]; % end point
 % t = dt*(0:K);
 [b2,a2] = butter(2,5*dt,'low'); 
 %% 2. screw
 t = 0:dt:K*dt;
-% p_screw = [x0(1)-50+50*cos(2*pi*t/150);
-%            x0(2)-50+50*sin(2*pi*t/150);
-%            x0(3) + (xg(3) - x0(3))*t.^2/t(end)^2;];
-% 
-% v_screw = [-50*(2*pi/150)*sin(2*pi*t/150);
-%            50*(2*pi/150)*cos(2*pi*t/150);
-%            2*(xg(3) - x0(3))*t/t(end)^2;]; 
-% u_screw = [-50*(2*pi/150)^2*cos(2*pi*t/150);
-%            -50*(2*pi/150)^2*sin(2*pi*t/150);
-%            2*(xg(3) - x0(3))*ones(1,length(t))/t(end)^2;];
-       
 p_screw = [x0(1)-50+50*cos(2*pi*t/150);
-           x0(2)-50+50*sin(2*pi*t/150);
-           x0(3) + (xg(3) - x0(3))*t.^3/t(end)^3;];
+           x0(2)+50*sin(2*pi*t/150);
+           x0(3) + (xg(3) - x0(3))*t.^2/t(end)^2;];
+
 v_screw = [-50*(2*pi/150)*sin(2*pi*t/150);
            50*(2*pi/150)*cos(2*pi*t/150);
-           3*(xg(3) - x0(3))*t.^2/t(end)^3;]; 
+           2*(xg(3) - x0(3))*t/t(end)^2;]; 
 u_screw = [-50*(2*pi/150)^2*cos(2*pi*t/150);
            -50*(2*pi/150)^2*sin(2*pi*t/150);
-           6*(xg(3) - x0(3))*t/t(end)^3;];
+           2*(xg(3) - x0(3))*ones(1,length(t))/t(end)^2;];
+       
+% p_screw = [x0(1)-50+50*cos(2*pi*t/150);
+%            x0(2)-50+50*sin(2*pi*t/150);
+%            x0(3) + (xg(3) - x0(3))*t.^3/t(end)^3;];
+% v_screw = [-50*(2*pi/150)*sin(2*pi*t/150);
+%            50*(2*pi/150)*cos(2*pi*t/150);
+%            3*(xg(3) - x0(3))*t.^2/t(end)^3;]; 
+% u_screw = [-50*(2*pi/150)^2*cos(2*pi*t/150);
+%            -50*(2*pi/150)^2*sin(2*pi*t/150);
+%            6*(xg(3) - x0(3))*t/t(end)^3;];
 
 % ------used to testify the v_screw-----%
 % figure(1)
@@ -68,15 +68,15 @@ for i = 2:K+1
 end
 
 %% 3.5 KF
-x0 = [x0(1:3);v_screw(:,1)] + [1 1 -1 0 0 0]';
+x0 = [p_screw(:,1);v_screw(:,1)] + [1 1 -1 0 0 0]';
 % x0(1) = x0(1)+5;
-x_KF = KF(u_screw(:,1:K),uwb,x0,dt,sigma_omega,sigma_v);
+[x_KF,x_pre] = KF2(u_screw(:,1:K),uwb,x0,dt,sigma_omega,sigma_v);
 
 figure(6)
-plot3(xt(1,:),xt(2,:),xt(3,:),'r',x0(1),x0(2),x0(3),'dg',xg(1),xg(2),xg(3),'*r')
+plot3(x_pre(1,:),x_pre(2,:),x_pre(3,:),'r')
 hold on 
 plot3(xt(1,:),xt(2,:),xt(3,:),'k--')
-legend('real trajectory','desired trajectory')
+legend('predict trajectory','desired trajectory')
 xlabel('x')
 ylabel('y')
 zlabel('z')
